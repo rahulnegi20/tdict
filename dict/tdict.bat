85590592f2e54088717754b49fd1313675f49b5c
SineshX <Sineshkumarr@gmail.com>
2021-10-05 18:31:23 +0530

    added windows bat script

---
+++ dict/tdict.bat
@@ -0,0 +1,10 @@
+@echo off
+:: note 1 - these must be should be added in environment variable : 
+::        - python
+::        - project folder (D:\Sinesh\Project\tdict)
+
+
+:: please replace ur path to the tdict.py 
+python "D:\Sinesh\Project\tdict\dict.py" %1
+
+::ye v mast hahi vidu :)
